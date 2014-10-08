1cab5322191b936ea9caa9c7ce41168a8a8b37ed
Mickael Istria <mistria@redhat.com>
2014-10-08 10:00:15 +0200

    Issue #5: fix filters when using working sets

--- plugins/org.jboss.tools.eclipse/src/org/jboss/tools/eclipse/nestedProjects/HideTopLevelProjectIfNested.java
+++ plugins/org.jboss.tools.eclipse/src/org/jboss/tools/eclipse/nestedProjects/HideTopLevelProjectIfNested.java
@@ -12,6 +12,9 @@ package org.jboss.tools.eclipse.nestedProjects;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.jface.viewers.TreeNode;
+import org.eclipse.jface.viewers.TreePath;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerFilter;
 import org.eclipse.ui.IWorkingSet;
@@ -26,9 +29,21 @@ public class HideTopLevelProjectIfNested extends ViewerFilter {
 	@Override
 	public boolean select(Viewer viewer, Object parentElement, Object element) {
 		if (element instanceof IProject) {
-			if (parentElement != null && (parentElement instanceof IWorkspaceRoot || parentElement instanceof IWorkingSet)) {
-				if (NestedProjectManager.isShownAsNested((IProject)element)) {
-					return false;
+			if (parentElement != null) {
+				Object parentObject = null;
+				if (parentElement instanceof TreeNode) {
+					parentObject = ((TreeNode)parentElement).getValue();
+				} else if (parentElement instanceof TreePath) {
+					parentObject = ((TreePath)parentElement).getLastSegment();
+				} else {
+					parentObject = parentElement;
+				}
+				if (parentObject instanceof IAdaptable) {
+					IAdaptable parentAdaptable = (IAdaptable)parentObject;
+					if (parentAdaptable.getAdapter(IWorkspaceRoot.class) != null ||
+						parentAdaptable.getAdapter(IWorkingSet.class) != null) {
+						return !NestedProjectManager.isShownAsNested((IProject)element);
+					}
 				}
 			}
 		}
