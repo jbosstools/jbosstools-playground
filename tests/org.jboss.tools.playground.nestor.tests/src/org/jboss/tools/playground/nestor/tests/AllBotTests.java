362fc440d630e27200429cd44883dc60a27c1121
Mickael Istria <mistria@redhat.com>
2014-11-10 20:10:41 +0100

    Beginning of integration-test

---
+++ tests/org.jboss.tools.playground.nestor.tests/src/org/jboss/tools/playground/nestor/tests/AllBotTests.java
@@ -0,0 +1,11 @@
+package org.jboss.tools.playground.nestor.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestNestedFolder.class })
+public class AllBotTests {
+
+}
