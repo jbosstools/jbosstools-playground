81c76254eb8726ab73e1f07c5a7f4f4eec558533
Mickael Istria <mistria@redhat.com>
2014-11-27 17:45:37 +0100

    Persist Nestor state in Preference

--- plugins/org.jboss.tools.playground.nestor/src/org/jboss/tools/playground/nestor/internal/ProjectPresentationHandler.java
+++ plugins/org.jboss.tools.playground.nestor/src/org/jboss/tools/playground/nestor/internal/ProjectPresentationHandler.java
@@ -4,31 +4,39 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.ui.navigator.resources.ProjectExplorer;
+import org.jboss.tools.playground.nestor.Activator;
 
 public class ProjectPresentationHandler extends AbstractHandler {
 
 	private static final String NEST_PARAMETER = "org.jboss.tools.playground.nestor.projectPresentation.nest"; //$NON-NLS-1$
-	private static boolean nest = false;
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+		if (HandlerUtil.matchesRadioState(event)) {
+			return null;
+		}
+	
+		Boolean previousNest = isNestingEnabled();
 		String newNestParam = event.getParameter(NEST_PARAMETER);
 		boolean newNest = false;
 		if (newNestParam != null) {
 			newNest = Boolean.parseBoolean(newNestParam);
 		}
-		if (newNest != nest) {
-			nest = newNest;
+		if (newNest != previousNest) {
+			Activator.getDefault().getPreferenceStore().setValue(NEST_PARAMETER, newNest);;
 			((ProjectExplorer)PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActivePart()).getCommonViewer().refresh();
 		}
+		
+		HandlerUtil.updateRadioState(event.getCommand(), Boolean.toString(newNest));
 		// TODO refresh selection
-		return Boolean.valueOf(nest);
+		return Boolean.valueOf(newNest);
 	}
 
 
 	public static boolean isNestingEnabled() {
-		return nest;
+		return Activator.getDefault().getPreferenceStore().getBoolean(NEST_PARAMETER);
 	}
 
 	@Override
